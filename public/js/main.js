1814f21b4b9526da528b9599334aca9836272ad6
leeladhar04 <99746458+leeladhar04@users.noreply.github.com>
2024-01-24 14:32:48 +0530

    included axios in index

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,9 @@ import * as constants from "./constants.js";
 import * as ui from "./ui.js";
 import * as recordingUtils from "./recordingUtils.js";
 import * as strangerUtils from "./strangerUtils.js";
-import axios from 'axios';
+// import axios from 'axios';
+
+const axios=require("axios");
 
 
 
